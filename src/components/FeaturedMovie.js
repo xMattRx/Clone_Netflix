13f46d52ab5bf01d640e7169522f0b68738f667b
Matthew Nascimento <matthewromie@hotmail.com>
2021-08-03 17:00:54 -0300

    corrigindo erros

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -17,34 +17,37 @@ export default ({ item }) => {
     }
 
     return (
-        <section className="featured" style={{
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        <div>
+            <section className="featured" style={{
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
 
-        }}>
-            <div className="featured--vertical">
-                <div className="featured--horizontal">
-                    <div className="featured--name">
-                        {item.original_name}
-                    </div>
-                    <div className="featured--info">
-                        <div className="featured--points">
-                            {item.vote_average} pontos
+            }}>
+                <div className="featured--vertical">
+                    <div className="featured--horizontal">
+                        <div className="featured--name">
+                            {item.original_name}
                         </div>
-                        <div className="featured--year">{firstDate.getFullYear()}</div>
-                        <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                        <div className="featured--info">
+                            <div className="featured--points">
+                                {item.vote_average} pontos
+                            </div>
+                            <div className="featured--year">{firstDate.getFullYear()}</div>
+                            <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+                        </div>
+                        <div className="featured--description">
+                            {description}
+                        </div>
+                        <div className="featured--buttons">
+                            <a className="featured--watchbutton" href={`/watch/${item.id}`}>► Assistir</a>
+                            <a className="featured--mylistbutton" href={`/list/add/${item.id}`}>+ Minha Lista</a>
+                        </div>
+                        <div className="featured--genres"><strong>Gêneros:</strong>{genres.join(', ')}</div>
                     </div>
-                    <div className="featured--description">
-                        {description}
-                    </div>
-                    <div className="featured--buttons">
-                        <a className="featured--watchbutton" href={`/watch/${item.id}`}>► Assistir</a>
-                        <a className="featured--mylistbutton" href={`/list/add/${item.id}`}>+ Minha Lista</a>
-                    </div>
-                    <div className="featured--genres"><strong>Gêneros:</strong>{genres.join(', ')}</div>
                 </div>
-            </div>
-        </section>
+            </section>
+        </div>
+
     )
 }
